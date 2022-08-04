fc85b3e5a7cf2bd0def63d000212fa898a85c918
WatchJani <jankokondic84@gmail.com>
2022-08-04 22:22:42 +0200

    fix bug

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,11 @@
 
 const Footer = () => {
     return (
-        <footer>Footer</footer>
+        <footer>
+            <p>DEV Community — A constructive and inclusive social network for software developers. With you every step of your journey.</p>
+            <p>Built on Forem — the open source software that powers DEV and other inclusive communities.</p>
+            <p>Made with love and Ruby on Rails. DEV Community © 2016 - 2022.</p>
+        </footer>
     )
 }
 
--- src/components/newsInfo/NewsInfo.jsx
+++ src/components/newsInfo/NewsInfo.jsx
@@ -3,49 +3,51 @@ import Styled from "./NewsInfo.module.css"
 const NewsInfo = () => {
     return (
         <div className={Styled.Info}>
-            <div className={Styled.PostByInfo}>
-                <div className={Styled.Image}>
-                    <img className={Styled.LogoBackGround} src="./images/logo2.webp" alt="" />
-                    <span>The DEV Team</span>
-                </div>
-                <button className={Styled.Follow}>Follow</button>
-                <p>The hardworking team behind dev.to ❤️</p>
-                <div>
-                    <p>Want to contribute to open source and help make the DEV community stronger?</p>
-                    <p>The code that powers DEV is called <b>Forem</b> and is freely available on GitHub.</p>
-                    <p>You're welcome to jump in!</p>
-                </div>
-                <button className={Styled.Contribute}>Contribute to Forem</button>
-            </div>
-            <div className={Styled.MorePost}>
-                <div className={Styled.Title}>
-                    <h3>
-                        More from <span>The DEV Team</span>
-                    </h3>
-                </div>
-                <div className={Styled.More}>
-                    <p className={Styled.TitleTag}>Redis x DEV Hackathon Help Thread</p>
-                    <div className={Styled.Tag}>
-                        <p>#redishackathon</p>
-                        <p>#redis</p>
-                        <p>#help</p>
+            <div className={Styled.Sticky}>
+                <div className={Styled.PostByInfo}>
+                    <div className={Styled.Image}>
+                        <img className={Styled.LogoBackGround} src="./images/logo2.webp" alt="" />
+                        <span>The DEV Team</span>
                     </div>
-                </div>
-                <div className={Styled.More}>
-                    <p className={Styled.TitleTag}>Community Discussion Thread — Redis x DEV Hackathon</p>
-                    <div className={Styled.Tag}>
-                        <p>#discuss</p>
-                        <p>#redishackathon</p>
-                        <p>#redis</p>
+                    <button className={Styled.Follow}>Follow</button>
+                    <p>The hardworking team behind dev.to ❤️</p>
+                    <div>
+                        <p>Want to contribute to open source and help make the DEV community stronger?</p>
+                        <p>The code that powers DEV is called <b>Forem</b> and is freely available on GitHub.</p>
+                        <p>You're welcome to jump in!</p>
                     </div>
+                    <button className={Styled.Contribute}>Contribute to Forem</button>
                 </div>
-                <div className={Styled.More}>
-                    <p className={Styled.TitleTag}>Follow Friday: UX/UI Edition (29 July 2022)</p>
-                    <div className={Styled.Tag}>
-                        <p>#meta</p>
-                        <p>#ux</p>
-                        <p>#ui</p>
-                        <p>#design</p>
+                <div className={Styled.MorePost}>
+                    <div className={Styled.Title}>
+                        <h3>
+                            More from <span>The DEV Team</span>
+                        </h3>
+                    </div>
+                    <div className={Styled.More}>
+                        <p className={Styled.TitleTag}>Redis x DEV Hackathon Help Thread</p>
+                        <div className={Styled.Tag}>
+                            <p>#redishackathon</p>
+                            <p>#redis</p>
+                            <p>#help</p>
+                        </div>
+                    </div>
+                    <div className={Styled.More}>
+                        <p className={Styled.TitleTag}>Community Discussion Thread — Redis x DEV Hackathon</p>
+                        <div className={Styled.Tag}>
+                            <p>#discuss</p>
+                            <p>#redishackathon</p>
+                            <p>#redis</p>
+                        </div>
+                    </div>
+                    <div className={Styled.More}>
+                        <p className={Styled.TitleTag}>Follow Friday: UX/UI Edition (29 July 2022)</p>
+                        <div className={Styled.Tag}>
+                            <p>#meta</p>
+                            <p>#ux</p>
+                            <p>#ui</p>
+                            <p>#design</p>
+                        </div>
                     </div>
                 </div>
             </div>
