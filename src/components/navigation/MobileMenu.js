5dff6e6921c937d29767201d654ed3defa925173
WatchJani <jankokondic84@gmail.com>
2022-08-14 14:29:13 +0200

    fix bug

--- src/components/navigation/MobileMenu.js
+++ src/components/navigation/MobileMenu.js
@@ -13,7 +13,7 @@ const PhoneMenu = ({ menuOpen }) => {
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="img" aria-labelledby="a3ibipw3sxyh3g0x41bze60g2kwcmxjq" aria-hidden="true" class="crayons-icon c-btn__icon"><title id="a3ibipw3sxyh3g0x41bze60g2kwcmxjq">Close</title><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636l4.95 4.95z"></path></svg>
                     </button>
                 </div>
-                <div className={Styled.MenuWrapper}>
+                <div className={Styled.MenuWrapper} onClick={() => { menuOpen() }}>
                     <Menu />
                 </div>
             </div>
